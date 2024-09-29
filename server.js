427f4ca4086bf7f8da6ce52b8213ab6359164c8c
Imane Hazim <imane.hazim@studio.unibo.it>
2024-09-29 13:41:00 +0200

    Fix: corrected the name of the page of the website

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'), (err) => {
-    if (err) {
-      res.status(err.status).send(err.message);
-    }
-  });
+  res.sendFile(path.join(__dirname, 'public', 'page1.html'));
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
